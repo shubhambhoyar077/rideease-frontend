97c6957721e62dc47de1947815b0b1d40c076687
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-07-12 22:37:27 +0300

    Added routes to the App and included redux store

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Cars from './components/Cars';
+import Reserve from './components/Reserve';
+import MyReservations from './components/MyReservations';
+import AddCars from './components/AddCars';
+import DeleteCars from './components/DeleteCars';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+
+  <>
+    <Navbar />
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Cars />} />
+        <Route path="/reserve" element={<Reserve />} />
+        <Route path="/myreservations" element={<MyReservations />} />
+        <Route path="/addcars" element={<AddCars />} />
+        <Route path="/deletecars" element={<DeleteCars />} />
+      </Routes>
     </div>
-  );
-}
-
+  </>
+);
 export default App;
