3acee730cfae298708a683e4ef235ccf8c9d3c6f
shubham <shubham.bhoyar077@gmail.com>
2023-07-13 10:37:37 +0530

    Add bootstrap package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
