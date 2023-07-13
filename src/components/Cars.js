f23480aefd81b2bbe865e671a0f293401704c3d8
unknown <sohailshahidi1435@gmail.com>
2023-07-13 15:17:35 +0430

    Display Cars data

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCars } from '../redux/cars/carsSlice';
+import '../styles/Cars.css';
 
 function Cars() {
   const cars = useSelector((state) => state.cars.cars) || [];
@@ -11,9 +12,18 @@ function Cars() {
   }, [dispatch]);
 
   return (
-    <section>
+    <section className="cars-section">
       <ul>
-        <li>cars</li>
+        {cars.map((car) => (
+          <li key={car.id}>
+            <div className="cars-card">
+              <img src={car.image} alt={car.name} />
+              <p>{car.name}</p>
+              <p>{car.price}</p>
+              <p>{car.details}</p>
+            </div>
+          </li>
+        ))}
       </ul>
 
       
