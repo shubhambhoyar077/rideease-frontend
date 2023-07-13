8520e561595b3eba1c7e857f69bb860d55f71fec
unknown <sohailshahidi1435@gmail.com>
2023-07-13 15:14:20 +0430

    Fetch Cars data

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCars } from '../redux/cars/carsSlice';
 
 function Cars() {
+  const cars = useSelector((state) => state.cars.cars) || [];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
+
   return (
-    <div>Cars</div>
+    <section>
+      <ul>
+        <li>cars</li>
+      </ul>
+
+      
+    </section>
   );
 }
 
