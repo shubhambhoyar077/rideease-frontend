7a4acb4ebabc587baa088f6499faea26acb8482a
shubham <shubham.bhoyar077@gmail.com>
2023-07-15 11:24:05 +0530

    Add forget and send conformation pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import DeleteCars from './components/DeleteCars';
 import Navbar from './components/Navbar';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ForgetPassword from './pages/ForgetPassword';
+import SendConfirmation from './pages/SendConfirmation';
 
 const App = () => (
   <>
@@ -19,8 +21,12 @@ const App = () => (
         <Route path="/myreservations" element={<MyReservations />} />
         <Route path="/addcars" element={<AddCars />} />
         <Route path="/deletecars" element={<DeleteCars />} />
+
+        {/* Auth pages routes:- */}
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/forget_password" element={<ForgetPassword />} />
+        <Route path="/send_conformation" element={<SendConfirmation />} />
       </Routes>
     </div>
   </>
---
+++ src/pages/SendConfirmation.js
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const SendConfirmation = () => {
+  const [isFormValid, setFormValid] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormValid(Boolean(formData.email));
+  };
+  return (
+    <div className="sign-container">
+      <h3>Send configuration email</h3>
+      <form className="mt-3 container-sm" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="d-flex gap-3">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={!isFormValid}
+          >
+            Send email
+          </button>
+          <Link to="/sign_in" className="btn btn-primary">
+            SignIn
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SendConfirmation;
