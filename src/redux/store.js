a815cb7b95485925b25acf8246d62ba6c874a3e8
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-07-17 16:40:31 +0300

    Updated the styles for Reservations

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice';
+import reservationsReducer from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     cars: carsReducer,
+    reservations: reservationsReducer,
   },
 });
 
