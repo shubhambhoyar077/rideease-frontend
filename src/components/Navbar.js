607ab5ca2b6012410f993e9494417b9e84a09a56
shubham <shubham.bhoyar077@gmail.com>
2023-07-17 20:29:22 +0530

    Add sign up and in btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Cross as Hamburger } from 'hamburger-react';
 import '../styles/Navbar.css';
+import { fetchAuth } from '../redux/auths/authsSlice';
 
 const Navbar = () => {
+  const { isAuth } = useSelector((state) => state.userAuth);
+  const { message, error } = useSelector((state) => state.auths);
+  const dispatch = useDispatch();
   const location = useLocation();
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 767.98);
+  const [isSmallScreen, setIsSmallScreen] = useState(
+    window.innerWidth <= 767.98,
+  );
 
+  console.log(isAuth);
   useEffect(() => {
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth <= 1100);
@@ -31,39 +39,123 @@ const Navbar = () => {
     setShowSidebar(!showSidebar);
   };
 
+  const handleLogout = () => {
+    const data = {
+      sign_in: false,
+      end_point: 'users/sign_out',
+      method_data: {
+        method: 'DELETE',
+        headers: {
+          Authorization: localStorage.getItem('authToken'),
+          'Content-Type': 'application/json',
+        },
+      },
+    };
+    dispatch(fetchAuth(data));
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <nav className={`col-lg-2 col-md-3 col-sm-4 col-12 bg-light sidebar ${showSidebar ? 'show' : ''}`}>
+        <nav
+          className={`col-lg-2 col-md-3 col-sm-4 col-12 bg-light sidebar ${
+            showSidebar ? 'show' : ''
+          }`}
+        >
           <div className="sidebar-sticky">
             <h1 className="main-title">RideEase</h1>
             <ul className="nav flex-column">
-              <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
-                <Link to="/" className="nav-link" onClick={() => setShowSidebar(false)}>
+              <li
+                className={`nav-item ${
+                  location.pathname === '/' ? 'active' : ''
+                }`}
+              >
+                <Link
+                  to="/"
+                  className="nav-link"
+                  onClick={() => setShowSidebar(false)}
+                >
                   Cars
                 </Link>
               </li>
-              <li className={`nav-item ${location.pathname === '/reserve' ? 'active' : ''}`}>
-                <Link to="/reserve" className="nav-link" onClick={() => setShowSidebar(false)}>
+              <li
+                className={`nav-item ${
+                  location.pathname === '/reserve' ? 'active' : ''
+                }`}
+              >
+                <Link
+                  to="/reserve"
+                  className="nav-link"
+                  onClick={() => setShowSidebar(false)}
+                >
                   Reserve
                 </Link>
               </li>
-              <li className={`nav-item ${location.pathname === '/myreservations' ? 'active' : ''}`}>
-                <Link to="/myreservations" className="nav-link" onClick={() => setShowSidebar(false)}>
+              <li
+                className={`nav-item ${
+                  location.pathname === '/myreservations' ? 'active' : ''
+                }`}
+              >
+                <Link
+                  to="/myreservations"
+                  className="nav-link"
+                  onClick={() => setShowSidebar(false)}
+                >
                   My Reservations
                 </Link>
               </li>
-              <li className={`nav-item ${location.pathname === '/addcars' ? 'active' : ''}`}>
-                <Link to="/addcars" className="nav-link" onClick={() => setShowSidebar(false)}>
+              <li
+                className={`nav-item ${
+                  location.pathname === '/addcars' ? 'active' : ''
+                }`}
+              >
+                <Link
+                  to="/addcars"
+                  className="nav-link"
+                  onClick={() => setShowSidebar(false)}
+                >
                   Add Cars
                 </Link>
               </li>
-              <li className={`nav-item ${location.pathname === '/deletecars' ? 'active' : ''}`}>
-                <Link to="/deletecars" className="nav-link" onClick={() => setShowSidebar(false)}>
+              <li
+                className={`nav-item ${
+                  location.pathname === '/deletecars' ? 'active' : ''
+                }`}
+              >
+                <Link
+                  to="/deletecars"
+                  className="nav-link"
+                  onClick={() => setShowSidebar(false)}
+                >
                   Delete Cars
                 </Link>
               </li>
             </ul>
+            <div className="d-flex gap-3">
+              {!isAuth && (
+                <>
+                  <Link to="sign_up" className="btn btn-primary">
+                    SignUp
+                  </Link>
+                  <Link to="sign_in" className="btn btn-primary">
+                    SignIn
+                  </Link>
+                </>
+              )}
+              {isAuth && (
+                <>
+                  <button
+                    type="button"
+                    onClick={handleLogout}
+                    className="btn btn-primary"
+                  >
+                    SignOut
+                  </button>
+                </>
+              )}
+            </div>
+            <small>{message}</small>
+            <small>{error}</small>
           </div>
         </nav>
         <main className="col-lg-10 col-md-9 col-sm-8 col-12">
