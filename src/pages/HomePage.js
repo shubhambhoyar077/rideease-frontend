ea07353c0fcb80c79d309a7b815d496345346aa9
unknown <sohailshahidi1435@gmail.com>
2023-07-17 18:56:50 +0430

    Add scrollRight function

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,14 +3,16 @@ import Cars from '../components/Cars';
 import '../styles/HomePage.css';
 
 const HomePage = () => {
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  const scrollRight = () => {
+    setCarouselIndex((prevIndex) => prevIndex + 3);
+  };
 
   return (
     <section className="home-page">
       <h1>LATEST MODELS</h1>
       <p className="home-page-text">Please select a Car Model</p>
-      <div className="carousel-container">
-        <Cars startIndex={carouselIndex} endIndex={carouselIndex + 3} />
-      </div>
     </section>
   );
 };
