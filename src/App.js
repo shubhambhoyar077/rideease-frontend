748a6005bfafbe663c3fa3c000f57b33ef7f2b1f
shubham <shubham.bhoyar077@gmail.com>
2023-07-18 13:05:23 +0530

    Add routes ass private route and fix nav bar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ForgetPassword from './pages/ForgetPassword';
 import SendConfirmation from './pages/SendConfirmation';
 import ConfirmationPage from './pages/ConfirmationPage';
 import SetPassword from './pages/SetPassword';
+import Private from './components/PrivateRoute';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -29,10 +30,38 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Navbar />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/reserve" element={<Reserve />} />
-            <Route path="/myreservations" element={<MyReservations />} />
-            <Route path="/addcars" element={<AddCars />} />
-            <Route path="/deletecars" element={<DeleteCars />} />
+            <Route
+              path="/reserve"
+              element={(
+                <Private>
+                  <Reserve />
+                </Private>
+              )}
+            />
+            <Route
+              path="/myreservations"
+              element={(
+                <Private>
+                  <MyReservations />
+                </Private>
+              )}
+            />
+            <Route
+              path="/addcars"
+              element={(
+                <Private>
+                  <AddCars />
+                </Private>
+              )}
+            />
+            <Route
+              path="/deletecars"
+              element={(
+                <Private>
+                  <DeleteCars />
+                </Private>
+              )}
+            />
 
             {/* Auth pages routes:- */}
             <Route path="/sign_in" element={<SignIn />} />
