597c7b9177000c037c496a5c743544b185c2c0d1
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-07-20 12:03:55 +0300

    Created a snapshop testing file for Cars(homepage) component

---
+++ src/__tests__/Cars.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+import Cars from '../components/Cars';
+
+jest.mock('react-redux', () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
+
+describe('Cars Component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render without crashing', () => {
+    useSelector.mockReturnValue([]);
+
+    useDispatch.mockReturnValue(jest.fn());
+
+    const { asFragment } = render(<Cars startIndex={0} endIndex={10} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+});
