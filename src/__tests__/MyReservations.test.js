030fd45f430085df576660dc0b52ede8ea5b7b78
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-07-20 12:04:59 +0300

    Created snapshot testing file for MyReservations component

---
+++ src/__tests__/MyReservations.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import MyReservations from '../components/MyReservations';
+
+const mockStore = configureMockStore([]);
+
+describe('MyReservations Component', () => {
+  it('should render correctly', () => {
+    // Mock the store state with any initial data the component needs
+    const store = mockStore({
+      reservations: {
+        reservations: [
+          {
+            service: { name: 'Test Service', image: 'test.jpg' },
+            reservation: { id: 1, city: 'Test City', date: '2023-07-15' },
+          },
+        ],
+      },
+    });
+    const tree = renderer.create(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
