8191d1b13f4f741da77fa1a9eb3a4571d88e37c0
unknown <sohailshahidi1435@gmail.com>
2023-07-26 13:40:26 +0430

    Fix Linters error

--- src/components/DeleteCars.js
+++ src/components/DeleteCars.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { fetchCars, deleteCar } from '../redux/cars/carsSlice';
 import '../styles/deleteCar.css';
 
